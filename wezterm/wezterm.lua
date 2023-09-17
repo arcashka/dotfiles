7fb9a3bd80f60fea763c8e409aab2dd44d9ed83f
arcashka <pewpewstol@gmail.com>
2023-09-17 13:23:33 +0300

    move standalone wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local config = {}
 
 config.color_scheme = "Vs Code Dark+ (Gogh)"
 config.font_size = 15.0
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("JetBrains Mono Nerd Font")
 config.hide_tab_bar_if_only_one_tab = true
 config.exit_behavior = "Close"
 config.window_close_confirmation = "NeverPrompt"
