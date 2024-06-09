dbae36a6c558a27d2012bd6bfd4b4920d8482c92
arcashka <pewpewstol@gmail.com>
2024-06-09 11:08:38 +0300

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,27 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = "Vs Code Dark+ (Gogh)"
+local function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Vs Code Dark+ (Gogh)"
+	else
+		return "Vs Code Light+ (Gogh)"
+	end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
 config.font_size = 15.0
-config.font = wezterm.font("JetBrains Mono Nerd Font")
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono Nerd Font",
+	"DengXian",
+})
 config.hide_tab_bar_if_only_one_tab = true
 config.exit_behavior = "Close"
 config.window_close_confirmation = "NeverPrompt"
